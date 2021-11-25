16404779e0237e7b59ac3b9bf1051e53f900cfea
bo.dong <bo.dong@bangcle.com>
2021-11-25 15:01:31 +0800

    feat: 提交测试文件

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,21 @@
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+test('hellow.vue', () => {
+  // 挂载组件
+  const wrapper = shallowMount(HelloWorld)
+  // wrapper.element.innerHTML
+  const a = wrapper.find('[data-id="data-id"]')
+  console.log(a.text())
+  expect(a.text()).toContain('ue-cli')
 })
+
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       propsData: { msg }
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
